2da5e409b57bde3992f2d6ec9547442798ed53ac
bclark288 <bclark288@gmail.com>
2019-10-01 14:35:44 -0400

    improvements to RMS, Zero Crossing Rate

--- ass2solution copy.py
+++ ass2solution copy.py
@@ -8,6 +8,22 @@ import math
 
 
 
+def window_audio(xb):
+    nbrBlocks = xb.shape[0]
+    blkSize = xb.shape[1]
+    rms = np.zeros(nbrBlocks)
+    w_audio = []
+    hwind = hann(blkSize)
+    for i in range(nbrBlocks):
+        wa = xb[i] * hwind
+        #wa = 0.5*(1 - np.cos(2*np.pi*xb[i]/(xb.shape[0] - 1)))
+        w_audio.append(wa)
+    wa = np.asarray(w_audio)
+    # plt.plot(wa_audio[0])
+    # plt.plot(hann(blkSize))
+    # plt.show()
+
+    return wa
 def block_audio(x,blockSize,hopSize,fs):
     numBlocks = math.ceil(x.size / hopSize)    
     xb = np.zeros([numBlocks, blockSize])       
@@ -20,58 +36,55 @@ def block_audio(x,blockSize,hopSize,fs):
     return (xb,t)
 
 def extract_spectral_centroid(xb, fs):
-    blockSize = xb.shape[1]
-    hannWindow = hann(blockSize, True)
-
-    spectralCentroids = []
-    for block in xb:
-        block *= hannWindow
-        blockFFT = np.abs(np.fft.fft(block)[:blockSize//2+1])        
-        spectralCentroids.append(np.sum(np.arange(blockFFT.size) * blockFFT) / np.sum(blockFFT))
-
-    return np.array(spectralCentroids) * fs / (blockSize)
+    wa = window_audio(xb)
+    centroid = []
+    for i in range(wa.shape[0]):
+        spectrum = abs(np.fft.fft(wa[i][:wa.shape[1]//2]))
+        #print("spgrm ",spgrm.shape)
+        freq_indices = np.arange(len(spectrum))
+        spectral_centroid = np.sum(freq_indices * spectrum)/np.sum(spectrum)
+        spectral_centroid = spectral_centroid*(fs/wa[i].size)
+        centroid.append(spectral_centroid)
+    centroid = np.asarray(centroid)
+    return centroid
 
 def extract_rms(xb):
-    blockSize = xb.shape[1]        
-    RMS = []
-    
-    for block in xb:
-        normalized_audio = block/np.max(abs(block))
-        blocksquare = np.sum(normalized_audio**2)
-        blockSize = 2*xb.shape[1]
-        blockRMS = np.sqrt(blocksquare/blockSize)
-        RMSdB = 20 * np.log10(blockRMS)
-        RMSdB = np.clip(RMSdB, a_min=-100, a_max=None)
-        RMS.append(RMSdB)
-    return np.array(RMS)
+    nbrBlocks = xb.shape[0]
+    rms = np.zeros(nbrBlocks)
+    for i in range(nbrBlocks):
+        rms[i] = np.sqrt(np.mean(np.power(xb[i],2)))
+    eps = 0.00001
+    for i in range(rms.shape[0]):
+        if rms[i] < eps:
+            rms[i] = eps
+    rms = 20*np.log10(rms)
+    for val in range(rms.shape[0]):
+        if rms[val] < -100:
+            rms[val] = -100
+    return rms
 
 def extract_zerocrossingrate(xb):
     nbrBlocks = xb.shape[0]
     zeroCrossingRate = np.zeros(nbrBlocks)
     for i in range(nbrBlocks):
-        x = np.sum(np.diff(np.sign(xb[i])) != 0)
-        y = 2*xb[i].size
-        zeroCrossingRate[i] = x/y
+        zeroCrossingRate[i] = 0.5*np.mean(np.abs(np.diff(np.sign(xb[i]))))
     return zeroCrossingRate
 
 def extract_spectral_crest(xb):
-    blockSize = xb.shape[1]
-    hannWindow = hann(blockSize, True)
-    spectralCrests = []
-    for block in xb:    
-        block *= hannWindow
-        blockFFT = np.abs(np.fft.fft(block)[:blockSize//2])        
-        spectralCrests.append(np.max(blockFFT) / np.sum(blockFFT))
-    return np.array(spectralCrests)
+    wa = window_audio(xb)
+    crests = []
+    for i in range(wa.shape[0]):
+        spgrm = abs(np.fft.fft(wa[i]))
+        crests.append(np.max(spgrm)/sum(spgrm))
+    crests = np.asarray(crests)
+    return crests
 
 def extract_spectral_flux(xb):
-    #wa = window_audio(xb)
-    blockSize = xb.shape[1]
-    hannWindow = hann(blockSize, True)
+    wa = window_audio(xb)
+    
     fluxs = []
-    for block in xb:
-        block *= hannWindow
-        spgrm = abs(np.fft.fft(block)[:blockSize//2])
+    for i in range(wa.shape[0]):
+        spgrm = abs(np.fft.fft(wa[i]))
         delta = np.diff(spgrm)
         flux = np.sqrt(np.sum(np.power(delta,2)))/delta.shape[0]
         fluxs.append(flux)
@@ -98,15 +111,18 @@ def aggregate_feature_per_file(features):
 def  get_feature_data(path, blockSize, hopSize):
     featureData = []
     for file in os.listdir(path):
-        #if file == 'marlene.wav':   
-        fs,data = wavfile.read(path + '/' + file)
-        features = extract_features(data,blockSize,hopSize,fs)
-        rosa_sig,rsr = librosa.core.load(path + '/' + file,sr=fs)
-        rosa_centroid = librosa.feature.spectral_centroid(rosa_sig,sr=fs,n_fft=blockSize,hop_length=hopSize)
-        rosa_rms = librosa.feature.rms(rosa_sig,frame_length=blockSize,hop_length=hopSize)
-        rosa_zcr = librosa.feature.zero_crossing_rate(rosa_sig,frame_length=blockSize,hop_length=hopSize)
-        agg_features = aggregate_feature_per_file(features)
-        featureData.append(agg_features)
+        if file == 'marlene.wav':   
+            fs,data = wavfile.read(path + '/' + file)
+            features = extract_features(data,blockSize,hopSize,fs)
+            rosa_sig,rsr = librosa.core.load(path + '/' + file,sr=fs)
+            rosa_centroid = librosa.feature.spectral_centroid(rosa_sig,sr=fs,n_fft=blockSize,hop_length=hopSize)
+            rosa_rms = librosa.feature.rms(rosa_sig,frame_length=blockSize,hop_length=hopSize)
+            rosa_zcr = librosa.feature.zero_crossing_rate(rosa_sig,frame_length=blockSize,hop_length=hopSize)
+            print("diff ",features[0], rosa_centroid)
+            print("diff rms",features[1],rosa_rms)
+            print("diff zcr ",features[2],rosa_zcr)
+            agg_features = aggregate_feature_per_file(features)
+            featureData.append(agg_features)
         
     featureData = np.asarray(featureData)
     return(featureData)
